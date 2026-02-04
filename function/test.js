701401d6888e98ddb5eb8b3553f538e8c3328312
CayoLoreti-dev <cayoloreti.dev@gmail.com>
2026-02-04 17:40:00 -0300

    function

---
+++ function/test.js
@@ -0,0 +1,16 @@
+/* Function
+
+    Trecho de código somente executado quando é chamado
+    */
+
+const variavel = 30;
+
+console.log(variavel);
+
+function nomeNaTela() {
+
+  console.log("Jesus");
+
+}
+
+nomeNaTela ()
